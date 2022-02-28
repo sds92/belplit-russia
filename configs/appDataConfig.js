81f81925c2ae92145fe3455481260f923978345a
sds92 <safronovds92@yandex.ru>
2022-02-28 15:42:11 +0300

    stable

--- configs/appDataConfig.js
+++ configs/appDataConfig.js
@@ -1,4 +1,3 @@
 export const appDataConfig = (appJsonArr, envURL) => {
-console.log("🚀 ~ file: appDataConfig.js ~ line 2 ~ appDataConfig ~ appJsonArr", appJsonArr)
   return appJsonArr.find(({url}) => url === envURL);
 };
