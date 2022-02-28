7dee8abea2cffc682d7db75d332c727f9807771d
sds92 <safronovds92@yandex.ru>
2022-02-28 17:58:21 +0300

    stable

--- components/complicated/Head/Head.js
+++ components/complicated/Head/Head.js
@@ -4,12 +4,18 @@ import Head from 'next/head';
 export default function HeadComponent({ script, head, children }) {
   return (
     <Head>
-      <title>{head?.title && head.title}</title>
+      <title>{head?.title ? head.title : 'Страница не найдена'}</title>
       <meta name='theme-color' content='#ffffff'></meta>
       {script && script}
       {head?.meta.map((item, index) => (
         <meta name={item.name} content={item.content} key={`META${index}`} />
       ))}
+      {!head && (
+        <>
+          <meta name={'keywords'} content={'Белтермо'} key={`META${index}`} />
+          <meta name={'description'} content={'Белтермо'} key={`META${index}`} />
+        </>
+      )}
       {children && children}
     </Head>
   );
