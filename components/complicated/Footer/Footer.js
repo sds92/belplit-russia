22a90a2618825310216e0b5f7a72af0de9fae84c
sds92 <safronovds92@yandex.ru>
2022-03-05 19:47:22 +0300

    stable

--- components/complicated/Footer/Footer.js
+++ components/complicated/Footer/Footer.js
@@ -23,13 +23,11 @@ export default function Footer(props) {
             {router.asPath !== '/catalog' && (
               <div className={`h-96 cursor-default`}>
                 <iframe
-                  src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.6768022748242!2d37.26887351592759!3d55.67722018053323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b550444d104d95%3A0x2e41f78026092c4!2z0JzQvtC70L7QtNGR0LbQvdCw0Y8g0YPQuy4sIDQ2LCDQntC00LjQvdGG0L7QstC-LCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LsuLCAxNDMwMDc!5e0!3m2!1sru!2sru!4v1605787923881!5m2!1sru!2sru'
+                  src={'https://api-maps.yandex.ru/frame/v1/-/CCUBEFtKWA'}
                   width='100%'
                   height='100%'
-                  aria-hidden='false'
-                  tabIndex='0'
-                  className={``}
-                  // style={{ borderRadius: '5px' }}
+                  frameBorder='1'
+                  allowFullScreen='true'
                 ></iframe>
               </div>
             )}
