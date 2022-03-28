19a9e8476668ec38a173183b847b5536f506e968
sds92 <safronovds92@yandex.ru>
2022-03-28 10:09:04 +0300

    stable

--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Layout } from '../../components/complicated';
 import fs from 'fs';
-// import { v2 } from 'utils/functions';
 
 export default function Catalog(props) {
   return <Layout.Catalog {...props} />;
